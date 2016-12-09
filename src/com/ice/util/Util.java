af2b4ef36a0c6cbe874718a94c1623880ba4468d
wangice <1163569270@qq.com>
2016-12-09 19:01:01 +0800

    工具类集合

---
+++ src/com/ice/util/Util.java
@@ -0,0 +1,32 @@
+package com.ice.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Properties;
+
+public class Util {
+	/**
+	 * 根据文件名获取配置文件
+	 * @param fileName
+	 * @return
+	 * @throws IOException
+	 */
+	public static Map<String, String> config(String fileName) throws IOException{
+		Map<String, String> configMap;
+		Properties config = new Properties();
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		InputStream inputStream = classLoader.getResourceAsStream(fileName);
+		config.load(inputStream);
+		Iterator<String> it = config.stringPropertyNames().iterator();
+		configMap = new HashMap<String, String>();
+		while (it.hasNext()) {
+			String key = it.next();
+			configMap.put(key, config.getProperty(key));
+		}
+		
+		return configMap;
+	}
+}
